9c4f9770030ecf3c9faaa671d69d3773873a41c3
SayanBiswas24 <sayan24982@gmail.com>
2025-12-18 20:04:57 +0530

    Project

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nagar_alert_app/screens/dashboard_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
